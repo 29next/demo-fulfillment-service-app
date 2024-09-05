bb50244fff893811b00f7eb236147f489990688f
Alex Phelps <alexphelps3@gmail.com>
2024-09-05 18:58:36 +0700

    test

--- app/app/settings.py
+++ app/app/settings.py
@@ -26,7 +26,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.getenv('SECRET_KEY', 'django-insecure-c5f%y-!bli)cn#eixeirw^s!5c72++zsw49!9-vr+x5u(w*fsg')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.getenv('DEBUG', True)
+DEBUG = False
 
 APP_DOMAIN = os.getenv('APP_DOMAIN', '')
 
