8d01b64ad561f1651e475f79d2b7dc4685987997
Alex Phelps <alexphelps3@gmail.com>
2024-09-05 18:53:50 +0700

    test

--- app/app/settings.py
+++ app/app/settings.py
@@ -148,14 +148,13 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
-
+STATIC_ROOT = str(BASE_DIR / "static")
 STATIC_URL = 'static/'
 
 
 # Media files
-
-MEDIA_URL = 'media/'
 MEDIA_ROOT = str(BASE_DIR / "media")
+MEDIA_URL = 'media/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
