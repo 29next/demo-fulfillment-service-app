a068ef87063e9cd3ecbc55a95ac72b3e64bac29f
Alex Phelps <alexphelps3@gmail.com>
2024-09-07 13:12:45 +0700

    try with debug true

--- app/app/urls.py
+++ app/app/urls.py
@@ -12,4 +12,9 @@ urlpatterns = [
     path('fulfillments/', include('fulfillments.urls', namespace='fulfillments')),
     path('', user_views.IndexView.as_view(), name='index'),
     path('admin/', admin.site.urls),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+]
+
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
